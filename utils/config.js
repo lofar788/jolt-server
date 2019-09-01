a8d56ea69fb89abce240f54f78f026f806a780bd
lofar788 <iansullivan8@gmail.com>
2019-09-01 16:07:48 -0400

    update utils

--- utils/config.js
+++ utils/config.js
@@ -1,15 +1,39 @@
 const config = {
+  server: {
+    secret: 'kjVkuti2xAyF3JGCzSZTk0YWM5JhI9mgQW4rytXc'
+  },
+  logType: 0,
+  auth: {
+    api : true,
+    api_user: 'admin',
+    api_pass: 'nms2018',
+  },
+  rtmp_server: {
     rtmp: {
       port: 1935,
       chunk_size: 60000,
       gop_cache: true,
-      ping: 30,
-      ping_timeout: 60
+      ping: 60,
+      ping_timeout: 30
     },
     http: {
       port: 8000,
+      mediaroot: '\\media',
       allow_origin: '*'
+    },
+    trans: {
+      ffmpeg: 'ffmpeg-4.1.3-win64-static\\bin\\ffmpeg.exe',
+      tasks: [
+        {
+          app: 'live',
+          hls: true,
+          hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+          dash: true,
+          dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+        }
+      ]
     }
-  };
- 
-  module.exports = config;
+  }
+};
+
+module.exports = config;
