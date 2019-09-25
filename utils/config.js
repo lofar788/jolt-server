0a8fe0d21b13dc10f638902d76407f93aa189530
lofar788 <iansullivan8@gmail.com>
2019-09-25 08:21:06 -0400

    added utils

--- utils/config.js
+++ utils/config.js
@@ -3,14 +3,14 @@ const config = {
     secret: 'kjVkuti2xAyF3JGCzSZTk0YWM5JhI9mgQW4rytXc'
   },
   logType: 0,
-  
+
   rtmp_server: {
 
-    auth: {
-      api : true,
-      api_user: 'admin',
-      api_pass: 'nms2018',
-    },
+    // auth: {
+    //   api : true,
+    //   api_user: 'admin',
+    //   api_pass: 'nms2018',
+    // },
     rtmp: {
       port: 1935,
       chunk_size: 60000,
@@ -20,19 +20,22 @@ const config = {
     },
     http: {
       port: 8000,
-      mediaroot: './media',
+      mediaroot: 'media',
+      // mediaroot: './media',
       allow_origin: '*'
     },
     trans: {
-      ffmpeg: '/usr/bin/ffmpeg',
-      // ffmpeg:'ffmpeg-4.1.3-win64-static\\bin\\ffmpeg.exe',
+      // ffmpeg: '/usr/bin/ffmpeg',
+      ffmpeg:'ffmpeg-4.1.3-win64-static\\bin\\ffmpeg.exe',
       tasks: [
         {
           app: 'live',
           hls: true,
           hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
           dash: true,
-          dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+          dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+          mp4: true,
+          mp4Flags: '[movflags=faststart]',
         }
       ]
     }
