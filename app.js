409e6b23fc73fc3a411e01c7fea53cad99d7a70d
lofar788 <iansullivan8@gmail.com>
2019-11-04 20:19:56 -0500

    hash streamkey

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const node_server = require('node-media-server')
+// const node_server = require('node-media-server')
+const node_server = require('./Node-Media-Server-master')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const axios = require('axios')
 const cors = require('cors')
 const { db } = require('./utils/firebase-config')
+const md5 = require('md5')
 
 app.get('/', (req, res) => {
 	return (res.json({ message: 'ey' }))
@@ -21,19 +23,19 @@ setInterval(function () {
 			let data = doc.data()
 			let streamKey = data['streamKey']
 			// axios.get('http://localhost:8000/api/streams/live/' + streamKey)
-				
+
 			axios.get('http://34.68.42.134:8000/api/streams/live/' + streamKey, {
-				
-			// axios.get('http://localhost:8000/api/streams/live/' + streamKey, {
-				auth:{
-				username: 'admin',
-				password: 'password'
+
+				// axios.get('http://localhost:8000/api/streams/live/' + streamKey, {
+				auth: {
+					username: 'admin',
+					password: 'password'
 				}
-			}).then((response) =>{
-				db.collection('users').doc(doc.id).update({viewers:response.data.viewers})
+			}).then((response) => {
+				db.collection('users').doc(doc.id).update({ viewers: response.data.viewers })
 				// doc.data().update({viewers:response.data.viewers})
 			}).catch(err => {
-				console.log(err.response)
+				console.log(err)
 			})
 			// doc.data().update({viewers})
 		})
@@ -45,8 +47,14 @@ setInterval(function () {
 
 nms.on('prePublish', async (id, StreamPath, args) => {
 	let stream_key = getStreamKeyFromStreamPath(StreamPath);
+	const session = nms.getSession(id)
+	// const path = session['publishStreamPath']
+	const newKey = StreamPath.split('/')
+	newKey[newKey.length-1] = md5(stream_key + 'password')
+	session['publishStreamPath'] = newKey.join('/')
 	db.collection('users').where('streamKey', '==', stream_key).limit(1).get().then((querySnapshot) => {
-
+		
+		
 		if (querySnapshot.empty) {
 			let session = nms.getSession(id);
 			session.reject();
